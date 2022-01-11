506e5d6e6f068c1cc61388c03721e50cbd60b954
Christopher <christopherwrose2@gmail.com>
2022-01-10 17:00:06 -0800

    assert equals program

---
+++ assertEquals.js
@@ -0,0 +1,12 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
