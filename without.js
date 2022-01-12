330756ae30f3c83b0e0e5bbe265a27b7e773b2dc
Christopher Rose <48345024+ChristopherRose13@users.noreply.github.com>
2022-01-11 20:31:52 -0800

    without update

---
+++ without.js
@@ -0,0 +1,15 @@
+const without = function (list, toBeRemoved) {
+  let newList = [...list];
+  for (let i = 0; i < newList.length; i ++) {
+    for (let j = 0; j < toBeRemoved.length; j ++) {
+      if (newList[i] === toBeRemoved[j]) {
+        newList.splice(i, 1);
+      }
+    }
+  }
+  return newList;
+};
+
+const words = ["hello", "world", "lighthouse"];
+console.log(without(words, ["lighthouse"]));
+console.log(without([1,2,3], [1,4,6]));
