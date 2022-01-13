1083cc04b7f55b14a6310579e328926d25a11084
Christopher Rose <48345024+ChristopherRose13@users.noreply.github.com>
2022-01-13 15:38:04 -0800

    assert obj equals func

--- assertObjectEquals.js
+++ assertObjectEquals.js
@@ -45,19 +45,10 @@ const eqObjects = function(object1, object2) {
   return true;
 };
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-eqObjects(ab, ba); // => true
-assertEqual(eqObjects(ab,ba), true);
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  console.log(`Example label: ${inspect(actual)}`);
+  assertEqual(eqObjects(actual, expected), true);
+};
 
-const abc = { a: "1", b: "2", c: "3" };
-eqObjects(ab, abc);
-assertEqual(eqObjects(ab,abc), false);
-
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-
-assertEqual(eqObjects(cd, dc), true); // => true
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false);
+assertObjectsEqual({a: "b"}, {a: "b"});
