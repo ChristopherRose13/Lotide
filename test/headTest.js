e104ca6997bf8f74c503d8a37ed8c4ae2f11fcd8
Christopher Rose <48345024+ChristopherRose13@users.noreply.github.com>
2022-01-17 14:58:57 -0800

    head chai and mocha update for test

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,26 @@
-const assertEqual = require('../assertEquals');
-const head = require('../head');
+// const assertEqual = require('../assertEquals');
+// const head = require('../head');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head([]), "Hello");
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head([]), "Hello");
+
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("returns 5 for [5,6,7]", () => {
+    assert.strictEqual(head([5,6,7]), 5);
+  });
+
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+});
