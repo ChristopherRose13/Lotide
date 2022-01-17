0ad7ec52f436c6e63e8765eb9add9d2ab0f10e6c
Christopher Rose <48345024+ChristopherRose13@users.noreply.github.com>
2022-01-17 15:53:04 -0800

    updated tail test and tail refactor

--- tail.js
+++ tail.js
@@ -8,9 +8,12 @@ const assertEqual = function(actual, expected) {
 
 const tail = function(arr) {
   if (arr.length > 1) {
-    return arr.slice(1);
+    return arr.slice(arr.length - 1)[0];
   }
   return [];
 };
-
+const result = tail(["Hello", "Lighthouse", "Labs", "code"]);
+console.log(result);
+//assertEqual(result.length, 4);
+//assertEqual(result, "Labs");
 module.exports = tail;
