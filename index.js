7120e5190ebdc73d00dc09fb4da387585215f415
Christopher Rose <48345024+ChristopherRose13@users.noreply.github.com>
2022-01-17 17:17:32 -0800

    new syntax

--- index.js
+++ index.js
@@ -16,20 +16,20 @@ const takeUntil = require('./takeUntil');
 const without = require('./without');
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  assertArraysEqual : assertArraysEqual,
-  assertEquals : assertEquals,
-  assertObjectEquals : assertObjectEquals,
-  countLetters : countLetters,
-  countOnly : countOnly,
-  eqArrays : eqArrays,
-  eqObjects : eqObjects,
-  findKey : findKey,
-  findKeyByValue : findKeyByValue,
-  letterPositions : letterPositions,
-  map : map,
-  takeUntil : takeUntil,
-  without : without
+  head,
+  tail,
+  middle,
+  assertArraysEqual,
+  assertEquals,
+  assertObjectEquals,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  letterPositions,
+  map,
+  takeUntil,
+  without
 };
